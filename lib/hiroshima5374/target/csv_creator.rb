fb550083624df6cfd2f2f851d5e5ae02358e6444
Tomohiko Himura <eiel.hal@gmail.com>
2014-03-08 20:41:34 +0900

    情報源の一覧を登録

--- lib/hiroshima5374/target/csv_creator.rb
+++ lib/hiroshima5374/target/csv_creator.rb
@@ -4,6 +4,13 @@ require 'fileutils'
 module Hiroshima5374::Target
   HTML_FILES = [
     "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277095003184/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277193991784/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277261742268/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277274582025/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277277472954/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277363424007/index.html",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277431455385/index.htmla",
+    "http://www.city.hiroshima.lg.jp/www/contents/0000000000000/1277431508075/index.html#ya",
   ]
 
   class CSVCreator
