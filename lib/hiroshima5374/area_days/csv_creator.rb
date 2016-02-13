e021a23741603a6a47995d3a7c665f157e0755e0
Tomohiko Himura <eiel.hal@gmail.com>
2016-02-14 08:49:35 +0900

    2016年度のデータへ修正

--- lib/hiroshima5374/area_days/csv_creator.rb
+++ lib/hiroshima5374/area_days/csv_creator.rb
@@ -1,27 +1,28 @@
+# -*- coding: utf-8 -*-
 require 'csv'
 require 'fileutils'
 require 'hiroshima5374/area_days/parser'
 
 module Hiroshima5374::AreaDays
   HTML_FILES = [
-    ["中区","http://www.city.hiroshima.lg.jp/www/contents/1422866937688/index.html"], # 中区1
-    ["中区","http://www.city.hiroshima.lg.jp/www/contents/1422869053822/index.html"], # 中区2
-    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1422869338945/index.html"], # 東区1
-    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1422869518269/index.html"], # 東区2
-    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1422870068198/index.html"], # 旧安芸町
-    ["南区","http://www.city.hiroshima.lg.jp/www/contents/1423109031382/index.html"], # 南区1
-    ["南区","http://www.city.hiroshima.lg.jp/www/contents/1423110057279/index.html"], # 南区2
-    ["西区","http://www.city.hiroshima.lg.jp/www/contents/1417480545521/index.html"], # 西区1
-    ["西区","http://www.city.hiroshima.lg.jp/www/contents/1423010293300/index.html"], # 西区2
-    ["安佐南区","http://www.city.hiroshima.lg.jp/www/contents/1423110315264/index.html"], # 安佐南区1
-    ["安佐南区","http://www.city.hiroshima.lg.jp/www/contents/1423787785455/index.html"], # 安佐南区2
-    ["安佐北区","http://www.city.hiroshima.lg.jp/www/contents/1423794228803/index.html"], # 安佐北区1
-    ["安佐北区","http://www.city.hiroshima.lg.jp/www/contents/1423715352189/index.html"], # 安佐北区2
-    ["安芸区","http://www.city.hiroshima.lg.jp/www/contents/1423804282224/index.html"], # 安芸区1
-    ["安芸区","http://www.city.hiroshima.lg.jp/www/contents/1424052025511/index.html"], # 安芸区2
-    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1424068549905/index.html"], # 佐伯区1
-    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1424133289921/index.html"], # 佐伯区2
-    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1424223165304/index.html"], # 佐伯区3
+    ["中区","http://www.city.hiroshima.lg.jp/www/contents/1456144057695/index.html"], # 中区1
+    ["中区","http://www.city.hiroshima.lg.jp/www/contents/1456144459889/index.html"], # 中区2
+    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1456361729117/index.html"], # 東区1
+    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1456145335329/index.html"], # 東区2
+    ["東区","http://www.city.hiroshima.lg.jp/www/contents/1456145516193/index.html"], # 旧安芸町
+    ["南区","http://www.city.hiroshima.lg.jp/www/contents/1456220835482/index.html"], # 南区1
+    ["南区","http://www.city.hiroshima.lg.jp/www/contents/1456221469862/index.html"], # 南区2
+    ["西区","http://www.city.hiroshima.lg.jp/www/contents/1456551088481/index.html"], # 西区1
+    ["西区","http://www.city.hiroshima.lg.jp/www/contents/1456222306031/index.html"], # 西区2
+    ["安佐南区","http://www.city.hiroshima.lg.jp/www/contents/1456222765714/index.html"], # 安佐南区1
+    ["安佐南区","http://www.city.hiroshima.lg.jp/www/contents/1456223516473/index.html"], # 安佐南区2
+    ["安佐北区","http://www.city.hiroshima.lg.jp/www/contents/1456223986267/index.html"], # 安佐北区1
+    ["安佐北区","http://www.city.hiroshima.lg.jp/www/contents/1456224409583/index.html"], # 安佐北区2
+    ["安芸区","http://www.city.hiroshima.lg.jp/www/contents/1456224852543/index.html"], # 安芸区1
+    ["安芸区","http://www.city.hiroshima.lg.jp/www/contents/1456225207410/index.html"], # 安芸区2
+    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1456225546686/index.html"], # 佐伯区1
+    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1456225859021/index.html"], # 佐伯区2
+    ["佐伯区","http://www.city.hiroshima.lg.jp/www/contents/1456226370823/index.html"], # 佐伯区3
   ]
 
   class CSVCreator
